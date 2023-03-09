7688d1f5c73bd9e5c73b6a2001d9bf0bd820ce91
SHIVAM SINGH <91870115+shivam8989@users.noreply.github.com>
2023-03-09 08:03:19 +0530

    Add files via upload
    Sum of the Nodes of a Binary Tree

---
+++ SumOfNodes.java
@@ -0,0 +1,35 @@
+package Binary_Tree;
+
+public class SumOfNodes {
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+        Node(int data){
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+    public static int Sum(Node root){
+        if(root==null){
+            return 0;
+        }
+        int leftsum = Sum(root.left);
+        int rightsum = Sum(root.right);
+
+        int treesum = leftsum+rightsum+root.data;
+        return treesum;
+
+    }
+    public static void main(String args []){
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.right = new Node(6);
+        System.out.println(Sum(root ));
+
+    }
+}
